7a0aa3fa0e525eccb4ae59ed16dbd7ce11c16886
Nakanz <nathan.hester@gmail.com>
2019-12-02 01:47:17 -0600

    Installations and client folder created

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ClientRappah</h1>
+    </div>
+  );
+}
+
+export default App;
