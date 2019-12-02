88c7b5ef626647381da9d0493779006f98755fa5
Nakanz <nathan.hester@gmail.com>
2019-12-02 15:11:34 -0600

    Added props to ProjectDetails

--- client/src/components/projects/ProjectDetails.js
+++ client/src/components/projects/ProjectDetails.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+    const id = props.match.params.id;
     return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card-title">supBitch</span>
+    <span className="card-title">supBitch - {id}</span>
                     <p>YUh yUh HUY</p>
                 </div>
                 <div className="card-action gret lighten-4 grey-text">
