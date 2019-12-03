4d3f424d914a1e1a3b8f98aa27c25fbbff63e611
Nakanz <nathan.hester@gmail.com>
2019-12-02 19:47:54 -0600

    Signin page created and routed

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import  Navbar  from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
 import './App.css';
 
 class App extends Component {
@@ -12,8 +13,9 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path='/project/:id' component={ProjectDetails} />
             <Route exact path='/' component={Dashboard} />
+            <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn}/>
           </Switch>
         </div>
       </BrowserRouter>
