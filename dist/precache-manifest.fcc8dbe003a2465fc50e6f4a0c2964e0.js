df6a0087f3f8f7add6338acef1906930431a54fe
Nakanz <nathan.hester@gmail.com>
2020-02-17 20:06:31 -0600

    moved build into dist folder for deployment

---
+++ dist/precache-manifest.fcc8dbe003a2465fc50e6f4a0c2964e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223d4f4bfcd61c70420f817fab96b00a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69ecbccedf65c6b8c801",
+    "url": "/static/css/main.bf254b0e.chunk.css"
+  },
+  {
+    "revision": "82f2731da566a881e624",
+    "url": "/static/js/2.6cabb8f2.chunk.js"
+  },
+  {
+    "revision": "cd82946ec0dbbe38c66eeb7386fbe6b9",
+    "url": "/static/js/2.6cabb8f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69ecbccedf65c6b8c801",
+    "url": "/static/js/main.40d585ca.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
